f53de05cf269c572fe333119ac0239882fd69897
Clark Hinchcliff <clarkhinchcliff@gmail.com>
2015-08-11 19:46:38 -0700

    Initial Commit

---
+++ shooter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  shooter
+//
+//  Created by clark hinchcliff on 8/11/15.
+//  Copyright (c) 2015 Clark Hinchcliff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ shooter/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  shooter
+//
+
+//  Copyright (c) 2015 Clark Hinchcliff. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
